27d436bdf28bf1108af16bb4e54b2a688f4e7fa5
Espen Henriksen <dev@henriksen.is>
2017-11-20 20:44:00 +0100

    Make getUri work in browser by using location.origin

--- utils/initApollo.js
+++ utils/initApollo.js
@@ -1,25 +1,29 @@
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import fetch from 'isomorphic-fetch'
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from 'isomorphic-fetch';
 
-let apolloClient = null
+let apolloClient = null;
 
 // Polyfill fetch() on the server (used by apollo-client)
 if (!process.browser) {
-  global.fetch = fetch
+  global.fetch = fetch;
+}
+
+/** Returns GraphQL endpoint URL. Server URL must be absolute */
+function getUri() {
+  if (process.browser) {
+    return `${window.location.origin}/graphql`;
+  }
+  return 'http://localhost:3000/graphql/';
 }
 
 function create (initialState) {
-  // Server URL must be absolute
-  let uri = process.env.EXTERNAL_URL
-    ? `${process.env.EXTERNAL_URL}/graphql`
-    : 'http://localhost:3000/graphql/';
   return new ApolloClient({
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri,
+      uri: getUri(),
       opts: {
         method: 'POST',
         credentials: 'same-origin',
@@ -33,13 +37,13 @@ export default function initApollo (initialState) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (!process.browser) {
-    return create(initialState)
+    return create(initialState);
   }
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = create(initialState)
+    apolloClient = create(initialState);
   }
 
-  return apolloClient
+  return apolloClient;
 }
