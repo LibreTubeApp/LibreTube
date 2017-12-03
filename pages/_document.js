ffbdfa18142b60bb592393d9e0d1d5b03222e490
Espen Henriksen <dev@henriksen.is>
2017-12-03 21:23:12 +0100

    Add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,10 @@ export default class CustomDocument extends Document {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" key="xUaCompat" />
           <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
           <meta charSet="utf-8" />
+          <link rel="icon" type="image/png" href="/static/favicon64.png" sizes="64x64" />
+          <link rel="icon" type="image/png" href="/static/favicon48.png" sizes="48x48" />
+          <link rel="icon" type="image/png" href="/static/favicon32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/static/favicon16.png" sizes="16x16" />
         </Head>
         <body>
           <Main />
